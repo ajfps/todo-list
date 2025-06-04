5c62e4e44af0391ec2904531a840eedd7351d9a9
Aaron Geron <geronaaronjoshua@gmail.com>
2025-06-04 22:16:27 +0800

    Added add a project functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { createElement } from "react";
 import "./style.css";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -75,13 +76,31 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     };
 
-    const dialog = document.querySelector("dialog");
-    const closeButton = document.querySelector("#close");
+    const dialog = document.querySelector(".create-todo");
+    const closeButton = document.querySelectorAll("#close");
     const submit = document.querySelector(".submit-todo");
     const title = document.querySelector("#title");
     const description = document.querySelector("#description");
     const dueDate = document.querySelector("#date");
-    submit.addEventListener("click", () => {
+
+    const projectTitle = document.querySelector("#project-title");
+    const addProjectBtn = document.querySelector(".add-project-btn");
+    const addProjectDialog = document.querySelector(".create-project");
+    const submitProject = document.querySelector(".submit-project");
+
+    addProjectBtn.addEventListener("click", () => {
+      addProjectDialog.showModal();
+    });
+
+    closeButton.forEach((closeBtn) =>
+      closeBtn.addEventListener("click", () => {
+        dialog.close();
+        addProjectDialog.close();
+      })
+    );
+
+    submit.addEventListener("click", (e) => {
+      e.preventDefault();
       const priorityInput = document.querySelector(
         'input[name="priority"]:checked'
       );
@@ -101,6 +120,98 @@ document.addEventListener("DOMContentLoaded", () => {
 
       dialog.close();
     });
+
+    submitProject.addEventListener("click", (e) => {
+      e.preventDefault();
+      ProjectManager.addProject(projectTitle.value);
+      refreshProjects();
+      console.log(ProjectManager.getProjects());
+    });
+
+    const projectNav = document.querySelector(".todo-project-nav");
+
+    const refreshProjects = () => {
+      const projects = ProjectManager.getProjects();
+
+      projects.forEach((project) => {
+        if (
+          project.name !== "Default Project" &&
+          project.name !== "Completed"
+        ) {
+          const projectRow = document.createElement("div");
+          projectRow.classList.add("todo-project-row");
+          const projectTitle = document.createElement("h3");
+          projectTitle.textContent = project.name;
+
+          projectNav.appendChild(projectRow);
+          projectRow.appendChild(projectTitle);
+
+          projectRow.addEventListener("click", () => {
+            projectTab(project.name);
+          });
+        }
+      });
+    };
+
+    const projectTab = (projectName) => {
+      content.innerHTML = "";
+      tabHeading.textContent = project.name;
+
+      const todosContainer = document.createElement("div");
+      todosContainer.classList.add("todos-container");
+      content.appendChild(todosContainer);
+
+      todosContainer.innerHTML = "";
+
+      const project = ProjectManager.getProject(projectName.name);
+      
+      project.todos.forEach((todo) => {
+        const titleText = document.createElement("h1");
+        const descriptionText = document.createElement("h2");
+        const dueDateText = document.createElement("h3");
+        const priorityText = document.createElement("h4");
+        const isCompleteText = document.createElement("h5");
+        const markCompleteBtn = document.createElement("button");
+
+        titleText.textContent = todo.title;
+        descriptionText.textContent = todo.description;
+        dueDateText.textContent = todo.dueDate;
+        priorityText.textContent = todo.priority;
+        isCompleteText.textContent = todo.status ? "Complete" : "Pending";
+        markCompleteBtn.textContent = "Mark Complete";
+
+        const textDetails = document.createElement("div");
+        textDetails.classList.add("todo-text-details");
+
+        const utilityDetails = document.createElement("div");
+        utilityDetails.classList.add("todo-utility-details");
+
+        const todoContainer = document.createElement("div");
+        todoContainer.classList.add("todo");
+
+        todosContainer.appendChild(todoContainer);
+        todoContainer.appendChild(textDetails);
+        todoContainer.appendChild(utilityDetails);
+
+        textDetails.appendChild(titleText);
+        textDetails.appendChild(descriptionText);
+
+        utilityDetails.appendChild(dueDateText);
+        utilityDetails.appendChild(priorityText);
+        utilityDetails.appendChild(isCompleteText);
+
+        markCompleteBtn.addEventListener("click", () => {
+          todo.status = true;
+          const completedProject = ProjectManager.getProject("Completed");
+          completedProject.addTodo(todo);
+          const index = project.todos.indexOf(todo);
+          project.deleteTodo(index);
+          refreshPendingTodo();
+        });
+
+        utilityDetails.appendChild(markCompleteBtn);
+      });
+    };
 
     const pendingTab = () => {
       content.innerHTML = "";
@@ -123,10 +234,6 @@ document.addEventListener("DOMContentLoaded", () => {
       content.appendChild(todosContainer);
       addTodoBtn.appendChild(addTodoIcon);
       addTodoBtn.appendChild(addTodoText);
-
-      closeButton.addEventListener("click", () => {
-        dialog.close();
-      });
 
       addTodoBtn.addEventListener("click", () => {
         dialog.showModal();
@@ -240,9 +347,10 @@ document.addEventListener("DOMContentLoaded", () => {
       refreshCompletedTodo();
     };
 
-    return { pendingTab, tabRefresher };
+    return { pendingTab, tabRefresher, refreshProjects };
   })();
 
   DOMController.pendingTab();
+  DOMController.refreshProjects();
   DOMController.tabRefresher();
 });
